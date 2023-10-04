1c1e64fe958f00cf2cb5c411da681f867ba7f2ed
MuhammadRehan05 <134819510+MuhammadRehan05@users.noreply.github.com>
2023-10-04 07:24:15 +0700

    Add files via upload

---
+++ source_code/soal_04.py
@@ -0,0 +1,15 @@
+class stack:
+    def_init_(self):
+    self_data = []
+
+def push(self, item):
+  self_data.append(item) 
+
+def pop(self):
+   if self is_empty():
+   raise empty('stack is Empty')
+retur self_data pop()
+
+def is_empty(self):
+return push(self_data.append)
+
